d1173999b100373cdcc54e0131c7cffd2c526d54
Rafael Luiz <luizrafael391@gmail.com>
2023-09-07 15:57:41 -0300

    fix(problem): use userId as parameter of ListProblems use case

--- src/modules/problem/domain/usecases/ListProblems.ts
+++ src/modules/problem/domain/usecases/ListProblems.ts
@@ -1,13 +1,9 @@
 import { Problem } from '@modules/problem/models';
 
 export interface ListProblems {
-  add(data: ListProblems.Params): Promise<ListProblems.Result>;
+  list(userId: string): Promise<ListProblems.Result>;
 }
 
 export namespace ListProblems {
-  export type Params = {
-    user_id: string;
-  };
-
   export type Result = Problem[];
 }
