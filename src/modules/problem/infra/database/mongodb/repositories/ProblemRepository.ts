08b1951a0a627edb69b540585848e680fe63397b
Rafael Luiz <luizrafael391@gmail.com>
2023-09-07 16:06:07 -0300

    chore(problems): remove repositories

--- src/modules/problem/infra/database/mongodb/repositories/ProblemRepository.ts
+++ src/modules/problem/infra/database/mongodb/repositories/ProblemRepository.ts
@@ -1,16 +1,13 @@
 import * as uuid from 'uuid';
 import MongoDb from '@shared/infra/database/mongodb';
 
-import { Problem } from '@modules/problem/models';
-import { IProblemRepository } from '@modules/problem/repositories';
-import { ICreateProblemDTO } from '@modules/problem/dtos';
 import {
   AddProblemRepository,
   ListProblemsRespository
 } from '@modules/problem/data/protocols';
 
 export class ProblemRepository
-  implements IProblemRepository, AddProblemRepository, ListProblemsRespository
+  implements AddProblemRepository, ListProblemsRespository
 {
   public async add(
     data: AddProblemRepository.Params
@@ -25,42 +22,9 @@ export class ProblemRepository
     return dbResult.acknowledged;
   }
 
-  public async list(userId: string): Promise<Problem[]> {
+  public async list(userId: string): Promise<ListProblemsRespository.Result> {
     const repository = MongoDb.getCollection('problems');
     const result = await repository.find({ user_id: userId }).toArray();
-
-    return result as any;
-  }
-
-  public async create(data: ICreateProblemDTO): Promise<Problem> {
-    const repository = MongoDb.getCollection('problems');
-    const problem = {
-      problem_id: uuid.v4(),
-      ...data
-    };
-
-    await repository.insertOne(problem);
-
-    return problem as any;
-  }
-
-  public async getAll(user_id: string): Promise<Problem[]> {
-    const repository = MongoDb.getCollection('problems');
-    const result = await repository.find({ user_id }).toArray();
-
-    return result as any;
-  }
-
-  public async getById(id: string): Promise<Problem> {
-    const repository = MongoDb.getCollection('problems');
-    const problem = await repository.findOne({ problem_id: id });
-
-    return problem as any;
-  }
-
-  public async getByCategory(category_id: number): Promise<Problem[]> {
-    const repository = MongoDb.getCollection('problems');
-    const result = await repository.find({ category_id }).toArray();
 
     return result as any;
   }
--- src/modules/problem/repositories/IProblemRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateProblemDTO } from '@modules/problem/dtos';
-import { Problem } from '@modules/problem/models';
-
-export interface IProblemRepository {
-  create(data: ICreateProblemDTO): Promise<Problem>;
-  getAll(user_id: string): Promise<Problem[]>;
-  getById(id: string): Promise<Problem | undefined>;
-  getByCategory(category_id: number): Promise<Problem[] | undefined>;
-}
