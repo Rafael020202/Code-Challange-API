af4cb65a9e273ce0feaa5b75781b8fbdfe82b307
Rafael Luiz <luizrafael391@gmail.com>
2023-09-07 13:35:54 -0300

    feat: add AddProblem usecase

---
+++ src/modules/problem/domain/usecases/AddProblem.ts
@@ -0,0 +1,21 @@
+export interface AddProblem {
+  add(data: AddProblem.Params): Promise<AddProblem.Result>;
+}
+
+export namespace AddProblem {
+  export type Params = {
+    title: string;
+    description: string;
+    input_description: string;
+    output_description: string;
+    category_id: number;
+    level: number;
+    inputs: {
+      value: string;
+      output: string;
+      is_example: boolean;
+    }[];
+  };
+
+  export type Result = boolean;
+}
