6a36c34fbee372751c000d736c0f2244fbd3d525
Rafael Luiz <luizrafael391@gmail.com>
2023-09-07 13:37:09 -0300

    refactor: remove tsrynge from CreateProblemService

--- src/modules/problem/services/CreateProblemService.ts
+++ src/modules/problem/services/CreateProblemService.ts
@@ -1,14 +1,8 @@
-import { inject, injectable } from 'tsyringe';
-
 import { ICreateProblemDTO } from '../dtos';
 import { IProblemRepository } from '../repositories';
 
-@injectable()
 export class CreateProblemService {
-  constructor(
-    @inject('ProblemRepository')
-    private problemRepository: IProblemRepository
-  ) {}
+  constructor(private problemRepository: IProblemRepository) {}
 
   public async execute(data: ICreateProblemDTO): Promise<void> {
     await this.problemRepository.create(data);
