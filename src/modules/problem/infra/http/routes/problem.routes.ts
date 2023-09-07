4939789c382fe2bdf6a3c5797b84945eb3b34661
Rafael Luiz <luizrafael391@gmail.com>
2023-09-07 13:40:28 -0300

    refactor: use AddProblemController to create a problem

--- src/modules/problem/infra/http/routes/problem.routes.ts
+++ src/modules/problem/infra/http/routes/problem.routes.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import { container } from 'tsyringe';
 
 import { ProblemController } from '@modules/problem/controllers';
+import { makeAddProblemController } from '@modules/problem/factories';
+import { adaptRoute } from '@shared/adapters';
 
+const addProblemController = makeAddProblemController();
 const problemController = container.resolve(ProblemController);
+
 const problemRoutes = Router();
 
-problemRoutes.post('/', problemController.create);
+problemRoutes.post('/', adaptRoute(addProblemController));
 problemRoutes.get('/', problemController.index);
 problemRoutes.get('/:id', problemController.get);
 
