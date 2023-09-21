297f06c51830f14922dad3f49175fdb8acefe092
Rafael Luiz <luizrafael391@gmail.com>
2023-09-21 09:57:25 -0300

    fix: fix typescript errors

--- src/modules/problem/data/protocols/db/LoadProblemsByAuthorRepository.ts
+++ src/modules/problem/data/protocols/db/LoadProblemsByAuthorRepository.ts
@@ -1,9 +1,9 @@
-import { ListProblems } from '@modules/problem/domain/usecases';
+import { LoadProblems } from '@modules/problem/domain/usecases';
 
 export interface LoadProblemsByAuthorRepository {
   loadByAuthor(author: string): Promise<LoadProblemsByAuthorRepository.Result>;
 }
 
 export namespace LoadProblemsByAuthorRepository {
-  export type Result = ListProblems.Result;
+  export type Result = LoadProblems.Result;
 }
